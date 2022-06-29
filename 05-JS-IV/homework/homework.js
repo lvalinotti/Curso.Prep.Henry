f4c63a5799b53b1e6975ab5f0b2eb14af74285d8
Leandro Valinotti <lvalinotti@gmail.com>
2022-06-29 16:39:44 -0300

    JSIV terminado y controlado

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -148,6 +148,12 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  producto['calcularPrecioDescuento'] = function(){
+    var descuento = 0;
+    descuento = this.precio - (this.precio * this.porcentajeDeDescuento);
+    return descuento;
+  }
+  return producto;
 }
 
 // No modificar nada debajo de esta línea
